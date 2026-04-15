6d11a575c6cbcbb0ccfbfb1d3d96a039afd19664
abtoy <unitedcoffee.hr@gmail.com>
2026-04-15 13:49:46 +0700

    Update server base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/tri/',
   server: {
-    base: '/tri/',   
     host: "::",
     port: 8080,
     hmr: {
